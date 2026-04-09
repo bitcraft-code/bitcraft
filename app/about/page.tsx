b811f1dfdf0a9a48f6899a9bc8da54c304ceebe0
bitcraft-code <bitcraft.code@gmail.com>
2026-04-09 17:11:59 +0100

    chore(ui): increase about card border stagger to 4000ms (one at a time)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -170,7 +170,7 @@ function PillarCardsGrid({ pillars }: { pillars: typeof COPY["en"]["pillars"] })
                 coneSpread={27}
                 fillOpacity={0}
                 animated
-                animationDelay={i * 1000}
+                animationDelay={i * 4000}
               >
                 <div className="flex gap-4 items-start p-5 text-left" style={{ transformStyle: "preserve-3d" }}>
                   <div
